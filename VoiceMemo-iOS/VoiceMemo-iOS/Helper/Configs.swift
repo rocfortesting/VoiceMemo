35ae17ad9a68d624b04d18257735bc580404415b
Roc <lighters9673@163.com>
2017-09-22 16:24:48 +0800

    [A] Configs.swift

---
+++ VoiceMemo-iOS/VoiceMemo-iOS/Helper/Configs.swift
@@ -0,0 +1,17 @@
+//
+//  Configs.swift
+//  VoiceMemo-iOS
+//
+//  Created by ROC Zhang on 2017/9/22.
+//  Copyright © 2017年 Roc Zhang. All rights reserved.
+//
+
+import UIKit
+
+struct Configs {
+    
+    struct UI {
+        static let basicLeading: CGFloat = 30
+    }
+    
+}
