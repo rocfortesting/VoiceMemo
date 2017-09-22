3f1c2662d1608db8073c4db0a5cedfea313bef84
Roc <lighters9673@163.com>
2017-09-22 16:06:46 +0800

    [A] UIColor & UIFont extensions

---
+++ VoiceMemo-iOS/VoiceMemo-iOS/Extensions/UIColor+VoiceMemo.swift
@@ -0,0 +1,16 @@
+//
+//  UIColor+VoiceMemo.swift
+//  VoiceMemo-iOS
+//
+//  Created by ROC Zhang on 2017/9/22.
+//  Copyright © 2017年 Roc Zhang. All rights reserved.
+//
+
+import UIKit
+
+extension UIColor {
+    
+    static let voiceMemoBackgroundColor = UIColor(red: 30/255, green: 30/255, blue: 40/255, alpha: 1)
+    static let voiceMemoTintColor = UIColor(red: 75/255, green: 140/255, blue: 245/255, alpha: 1)
+    
+}
---
+++ VoiceMemo-iOS/VoiceMemo-iOS/Extensions/UIFont+VoiceMemo.swift
@@ -0,0 +1,15 @@
+//
+//  UIFont+VoiceMemo.swift
+//  VoiceMemo-iOS
+//
+//  Created by ROC Zhang on 2017/9/22.
+//  Copyright © 2017年 Roc Zhang. All rights reserved.
+//
+
+import UIKit
+
+extension UIFont {
+    
+    static let voiceMemoTitleFont = UIFont.systemFont(ofSize: 50, weight: .bold)
+    
+}
