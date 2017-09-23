e56423c56eac74669611cbf898441721bbfd240d
Roc <lighters9673@163.com>
2017-09-23 23:48:28 +0800

    [A] UIImage extension

---
+++ VoiceMemo-iOS/VoiceMemo-iOS/Extensions/UIImage+VoiceMemo.swift
@@ -0,0 +1,17 @@
+//
+//  UIImage+VoiceMemo.swift
+//  VoiceMemo-iOS
+//
+//  Created by ROC Zhang on 2017/9/22.
+//  Copyright © 2017年 Roc Zhang. All rights reserved.
+//
+
+import UIKit
+
+extension UIImage {
+    
+    static let voiceMemoIconRecord = #imageLiteral(resourceName: "icon_record")
+    static let voiceMemoIconPlay = #imageLiteral(resourceName: "icon_play")
+    static let voiceMemoIconStop = #imageLiteral(resourceName: "icon_stop")
+    
+}
